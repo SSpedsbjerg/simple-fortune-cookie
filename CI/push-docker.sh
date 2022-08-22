aacfe7bb5e4e1950781959361d0d64fe51307d41
SSpedsbjerg <sispe20@student.sdu.dk>
2022-08-22 14:23:22 +0200

    Remove unused files

--- CI/push-docker.sh
+++
@@ -1,4 +0,0 @@
-echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/$docker_repo:1.0-${GIT_COMMIT::8}" 
-docker push "$docker_username/$docker_repo:latest" &
-wait
